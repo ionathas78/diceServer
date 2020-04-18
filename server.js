57102434a9aabfdffb97920a565b421fd4709c69
Jonathan Andrews <ionathas78@hotmail.com>
2020-04-18 00:36:44 -0500

    trying to fix external deployment

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("./rollFunctions.js");
 const express = require("express");
 const fs = require("fs");
 
-const app = new express();
-const PORT = 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
